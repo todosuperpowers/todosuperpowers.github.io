243d0b7b607c79d930491fb376bda92678a13d03
Todo Superpowers <dev@todosuperpowers.com>
2023-05-14 10:18:12 +0100

    chore: Upgrade next from v12 to v13, closes #141

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: '',
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
